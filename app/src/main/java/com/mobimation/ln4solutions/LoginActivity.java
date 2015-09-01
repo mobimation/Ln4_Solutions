ea7e75852afe5588a788ec58909c11d02fe668af
mobimation <gunnar.forsgren@mobimation.com>
2015-09-01 19:26:13 +0200

    First Alpha

--- app/src/main/java/com/mobimation/ln4solutions/LoginActivity.java
+++ app/src/main/java/com/mobimation/ln4solutions/LoginActivity.java
@@ -3,8 +3,8 @@ package com.mobimation.ln4solutions;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -17,34 +17,21 @@ import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends Activity {
     protected Button buttonSubmit;
     protected TextView status;
+    private final String LTAG = LoginActivity.class.getSimpleName();
     private static String url="https://h01.ln4solutions.com/code2url/code2url.php";
     private String webUrl;
 
@@ -58,20 +45,30 @@ public class LoginActivity extends Activity {
         status.setText(" ");
         final EditText customerId = (EditText)findViewById(R.id.customerId);
 
-        // Assemble and format customer id based on key input
-        customerId.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                return false;
-            }
-        });
-        buttonSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String id = customerId.getText().toString();
-                new Authenticator(getApplicationContext()).execute(url, id);
-            }
-        });
+        // Get granted URL if any and if so launch browser directly
+        final SharedPreferences pref = this.getSharedPreferences(
+                "com.mobimation.URL", Context.MODE_PRIVATE);
+        String u=pref.getString("url",null);
+        if (u!=null) {
+            launchWeb(getApplicationContext(), u);
+            finish(); // Abort Login activity launch
+        }
+        else {
+            // Assemble and format customer id based on key input
+            customerId.setOnKeyListener(new View.OnKeyListener() {
+                @Override
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    return false;
+                }
+            });
+            buttonSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String id = customerId.getText().toString();
+                    new Authenticator(getApplicationContext()).execute(url, id);
+                }
+            });
+        }
     }
 
     @Override
@@ -88,7 +85,6 @@ public class LoginActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -103,8 +99,8 @@ public class LoginActivity extends Activity {
     protected class Authenticator extends AsyncTask<String, String, String> {
 
         private final String TAG = Authenticator.class.getSimpleName();
-
-        Context context;
+        private Context context;
+        private String code;
 
         public String convertStreamToString(InputStream is) {
     /*
@@ -134,23 +130,24 @@ public class LoginActivity extends Activity {
         }
 
         protected Authenticator(Context context) {
-            this.context = context.getApplicationContext();
+            this.context = context;
         }
 
         @Override
         protected String doInBackground(String... params) {
-            Log.d(TAG, "doInBackground");
             String webUrl=null;
+            Log.d(TAG, "doInBackground");
             publishProgress("Submitting..");
 
            /**
             * Submit JSON request over https to Ln4 Solutions server
             */
+            code=params[1];
             HttpClient client = new DefaultHttpClient();
             HttpPost post = new HttpPost(params[0]);
             try {
                 JSONObject json = new JSONObject();
-                json.put("code", params[1]);
+                json.put("code", code);
                 HttpPost request = new HttpPost(params[0]);
                 request.setEntity(new ByteArrayEntity(json.toString().getBytes(
                         "UTF8")));
@@ -162,7 +159,6 @@ public class LoginActivity extends Activity {
                 String result=jsonObj.getString("result");
                 if (result.equals("Success")) {
                     webUrl = jsonObj.getString("url");
-                    Log.d(TAG,"Valid customer, URL="+webUrl);
                     publishProgress("Success");
                 }
                 else {
@@ -187,15 +183,42 @@ public class LoginActivity extends Activity {
         @Override
         protected void onPostExecute(String webUrl) {
             if (webUrl!=null) {
+                // Store granted URL
+                SharedPreferences pref = context.getSharedPreferences(
+                        "com.mobimation.URL", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("url",webUrl);
+                editor.commit();
                 // Launch WebActivity
                 Log.d(TAG,"Launching web activity");
                 status.setText("Loading browser..");
-                Intent intent = new Intent(context, WebActivity.class);
+                launchWeb(context,webUrl);
+                /* Intent intent = new Intent(context, WebActivity.class);
                 intent.putExtra("webUrl", webUrl);
-                startActivity(intent);
+                startActivity(intent); */
             }
             else
-                status.setText("Customer id not valid");
+                status.setText("Customer id "+code+" not valid");
         }
+    }
+
+    /**
+     * Launch Web browser activity
+     * @param c  Application context
+     * @param url  URLfor start page
+     */
+    private void launchWeb(Context c,String url) {
+        if (url !=null) {
+            Intent intent = new Intent(c, WebActivity.class);
+            // Clear activity stack so that back button on browser
+            // view will cause app exit instead of return to login.
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            intent.putExtra("webUrl", url); // Pass URL to browser activity
+            startActivity(intent);
+            finish();
+        }
+        else
+            Log.e(LTAG,"Error-launchWeb() called with null URL !");
     }
 }
