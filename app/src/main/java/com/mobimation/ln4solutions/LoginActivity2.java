9a7c92a9839384403867ee3c9741df823cb29658
mobimation <gunnar.forsgren@mobimation.com>
2015-09-07 11:44:56 +0200

    Alpha 8 - Reverted unsuccessful remote browser full screen settings

--- app/src/main/java/com/mobimation/ln4solutions/LoginActivity2.java
+++ app/src/main/java/com/mobimation/ln4solutions/LoginActivity2.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.SpannableString;
@@ -39,7 +40,7 @@ import java.io.InputStreamReader;
  * Experimental version of LoginActivity that makes use of the
  * custom formatting CustomerId widget.
  */
-public class LoginActivity2 extends Activity implements View.OnKeyListener {
+public class LoginActivity2 extends Activity {
     protected Button buttonSubmit;
     protected TextView status;
     private final String LTAG = LoginActivity2.class.getSimpleName();
@@ -49,7 +50,22 @@ public class LoginActivity2 extends Activity implements View.OnKeyListener {
     private int count=0;
     private int aft;
     private int bef;
-
+/*
+    @Override
+    public void onStart() {
+        super.onStart();
+        final View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= 11)
+        decorView.setOnSystemUiVisibilityChangeListener(
+                new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int i) {
+                        int height = decorView.getHeight();
+                        Log.i(LTAG, "Current height: " + height);
+                    }
+                });
+    }
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -125,9 +141,6 @@ public class LoginActivity2 extends Activity implements View.OnKeyListener {
         s.setSpan(new ForegroundColorSpan(Color.parseColor("#DDDDDD")), 0, s.length(), 0);
         item.setTitle(s);
 
-
-
-
         return true;
     }
 
@@ -148,6 +161,22 @@ public class LoginActivity2 extends Activity implements View.OnKeyListener {
         return super.onOptionsItemSelected(item);
     }
 
+/*
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            final View decorView = getWindow().getDecorView();
+            if (Build.VERSION.SDK_INT >= 11)
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
+*/
     private void VKdown() {
         InputMethodManager inputMethodManager =
                 (InputMethodManager) getApplicationContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
@@ -157,16 +186,6 @@ public class LoginActivity2 extends Activity implements View.OnKeyListener {
         if (view != null) {
             inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
-    }
-
-    @Override
-    public boolean onKey(View v, int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU) {
-            if (event.getAction()==KeyEvent.ACTION_UP)
-              return true;
-            return false;
-        } else
-        return false;
     }
 
     /**
@@ -307,8 +326,55 @@ public class LoginActivity2 extends Activity implements View.OnKeyListener {
             /**
              * Launch external web browser
              */
+ //         setImmersive();
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             startActivity(browserIntent);
         }
+    }
+
+
+    public void setImmersive() {
+
+        // BEGIN_INCLUDE (get_current_ui_flags)
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        // END_INCLUDE (get_current_ui_flags)
+        // BEGIN_INCLUDE (toggle_ui_flags)
+        /*
+        boolean isImmersiveModeEnabled =
+                ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+        if (isImmersiveModeEnabled) {
+            Log.i(LTAG, "Turning immersive mode mode off. ");
+        } else {
+            Log.i(LTAG, "Turning immersive mode mode on.");
+        }
+*/
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+        if (Build.VERSION.SDK_INT >= 11)
+         getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+        //END_INCLUDE (set_ui_flags)
     }
 }
